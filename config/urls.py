1c2d99c7f316f8f47fca049bb4e92b5c1b1923ee
seungsura <90497686+seungsura@users.noreply.github.com>
2023-04-22 13:29:24 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
+from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
-from drf_yasg       import openapi
+from drf_yasg import openapi
 
 
 urlpatterns = [
